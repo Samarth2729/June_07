4b7ab5cea01f80884200574283dc818a07b0a280
samar <samarthrathore2729@gmail.com>
2024-06-08 21:03:03 +0530

    Learning_001.py Learning_002.py Learning_003.py

---
+++ python3x/Learning_003.py
@@ -0,0 +1,25 @@
+# Esccape Seq
+print("Hello \"World\" ")
+print("hello \nworld")
+# List of shopping list
+# milk, sugar,poha,rice,oil,salt
+# 1. Add item
+# 2. Remove item
+# 3. Update item
+# 4. View item
+# 5. Exit
+# 6. Clear list
+# 7. Print list
+# 8. Search item
+# 9. Sort list
+# 10. Reverse list
+shopping_list = ["milk","sugar","poha","rice","butter"]
+print(shopping_list)
+shopping_list.append("cold drink")
+print(shopping_list)
+shopping_list.remove("rice")
+print(shopping_list)
+shopping_list.insert(3,"oil")
+print(shopping_list)
+shopping_list.pop()
+print(shopping_list)
