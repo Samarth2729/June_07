43a07a243098bd4f02e25e2346e2db7db9e6a2ae
samar <samarthrathore2729@gmail.com>
2024-06-08 20:29:05 +0530

    Learning_001.py Learning_002.py

--- python3x/Learning_001.py
+++ python3x/Learning_001.py
@@ -1,1 +1,17 @@
-print("Samarth")
+#Some important in built functions in python.
+#print(),input(),len(),type(),range(),min(),max().
+
+#Strings
+name = "Amit"
+print(name)
+print(type(name))
+print(len(name))
+print(id(name))
+#lenght of string
+name = name.upper()
+print(name)
+name = name.lower()
+print(name)
+name = name.casefold()
+print(name)
+
---
+++ python3x/Learning_002.py
@@ -0,0 +1,13 @@
+name = "This is a big line"
+print(name)
+print(len(name))
+print(name[12])
+
+val = None
+#Data type - Nonetype
+print(val)
+#None is nothing
+#None is not 0
+#None is not False
+#None is not empty string
+
